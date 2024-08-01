be894677e04645a579bf1f3d55021515dbdae5d4
LinuxChar <111671300+CharlieFISI@users.noreply.github.com>
2024-08-01 00:20:44 -0500

    Arreglo de opacidad de los ToDos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,10 +4,11 @@ import { updateTodoStatus } from '../data';
 
 const TodoItem = (props) => {
   const completedClass =
-    props.status === 'completed' ? 'bg-gray-200 opacity-30' : 'bg-gray-100';
+    props.status === 'completed' ? 'bg-gray-200 bg-opacity-40' : 'bg-gray-100';
 
-  const handleCheckboxChange = async () => {
-    const newStatus = props.status === 'completed' ? 'pending' : 'completed';
+  const handleCheckboxChange = async (event) => {
+    const isChecked = event.target.checked;
+    const newStatus = isChecked ? 'completed' : 'pending';
 
     const result = await updateTodoStatus(props.id, newStatus);
 
@@ -26,26 +27,29 @@ const TodoItem = (props) => {
         <div className="flex items-center">
           <Button
             as="span"
-            className="cursor-pointer border-none bg-transparent text-lg font-bold text-gray-600 hover:text-gray-700"
+            className="cursor-pointer border-none bg-transparent text-lg font-bold text-gray-400"
           >
             <FaBars />
           </Button>
           <Checkbox
-            className="mr-2 focus:ring-transparent"
+            className={'mr-2 focus:ring-transparent'}
             defaultChecked={props.status === 'completed'}
             onChange={handleCheckboxChange}
+            color="dark"
           />
-          <Label className="relative -top-px max-w-[calc(100%-40px)] break-words">
+          <Label
+            className={`relative -top-px max-w-[calc(100%-40px)] break-words ${props.status === 'completed' ? 'line-through opacity-30' : ''}`}
+          >
             {props.description}
           </Label>
         </div>
         <Dropdown
-          label={<FaEllipsisH />}
+          label={<FaEllipsisH className="text-gray-600" />}
           dismissOnClick={false}
           placement="right-start"
           inline
           arrowIcon={false}
-          className="ml-2"
+          className={'ml-2'}
         >
           <Dropdown.Item icon={FaEdit} onClick={() => alert('Editando')}>
             Editar
