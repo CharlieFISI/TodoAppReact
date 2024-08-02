fe4db261ea592a927d4967367a6a6d7496e81f84
LinuxChar <111671300+CharlieFISI@users.noreply.github.com>
2024-08-02 00:45:51 -0500

    Uso del drag and drop

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,16 @@
 import { Button, Checkbox, Label, Dropdown } from 'flowbite-react';
 import { FaEllipsisH, FaBars, FaEdit, FaTrashAlt } from 'react-icons/fa';
 import { updateTodoStatus } from '../data';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { restrictToParentElement } from '@dnd-kit/modifiers';
 
 const TodoItem = (props) => {
   const completedClass =
     props.status === 'completed' ? 'bg-gray-200 bg-opacity-40' : 'bg-gray-100';
 
-  const handleCheckboxChange = async (event) => {
-    const isChecked = event.target.checked;
+  const handleCheckboxChange = async (e) => {
+    const isChecked = e.target.checked;
     const newStatus = isChecked ? 'completed' : 'pending';
 
     const result = await updateTodoStatus(props.id, newStatus);
@@ -19,47 +22,60 @@ const TodoItem = (props) => {
     }
   };
 
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id: props.id,
+      modifiers: [restrictToParentElement]
+    });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  };
+
   return (
-    <li>
-      <div
-        className={`my-1 flex w-full items-center justify-between break-words rounded-md bg-white py-1 pl-px pr-2.5 shadow-sm ${completedClass}`}
-      >
-        <div className="flex items-center">
-          <Button
-            as="span"
-            className="cursor-pointer border-none bg-transparent text-lg font-bold text-gray-400"
-          >
-            <FaBars />
-          </Button>
-          <Checkbox
-            className={'mr-2 focus:ring-transparent'}
-            defaultChecked={props.status === 'completed'}
-            onChange={handleCheckboxChange}
-            color="dark"
-          />
-          <Label
-            className={`relative -top-px max-w-[calc(100%-40px)] break-words ${props.status === 'completed' ? 'line-through opacity-30' : ''}`}
-          >
-            {props.description}
-          </Label>
-        </div>
-        <Dropdown
-          label={<FaEllipsisH className="text-gray-600" />}
-          dismissOnClick={false}
-          placement="right-start"
-          inline
-          arrowIcon={false}
-          className={'ml-2'}
+    <div
+      style={style}
+      ref={setNodeRef}
+      {...attributes}
+      className={`my-1 flex w-full cursor-default items-center justify-between break-words rounded-md bg-white py-1 pl-px pr-2.5 shadow-sm ${completedClass}`}
+    >
+      <div className="flex items-center">
+        <Button
+          {...listeners}
+          as="span"
+          className="cursor-pointer border-none bg-transparent text-lg font-bold text-gray-400"
         >
-          <Dropdown.Item icon={FaEdit} onClick={() => alert('Editando')}>
-            Editar
-          </Dropdown.Item>
-          <Dropdown.Item icon={FaTrashAlt} onClick={() => alert('Eliminando')}>
-            Eliminar
-          </Dropdown.Item>
-        </Dropdown>
+          <FaBars />
+        </Button>
+        <Checkbox
+          className={'mr-2 cursor-pointer focus:ring-transparent'}
+          defaultChecked={props.status === 'completed'}
+          onChange={handleCheckboxChange}
+          color="dark"
+        />
+        <Label
+          className={`relative -top-px max-w-[calc(100%-40px)] break-words ${props.status === 'completed' ? 'line-through opacity-30' : ''}`}
+        >
+          {props.description}
+        </Label>
       </div>
-    </li>
+      <Dropdown
+        label={<FaEllipsisH className="text-gray-600" />}
+        dismissOnClick={false}
+        placement="right-start"
+        inline
+        arrowIcon={false}
+        className={'ml-2'}
+      >
+        <Dropdown.Item icon={FaEdit} onClick={() => alert('Editando')}>
+          Editar
+        </Dropdown.Item>
+        <Dropdown.Item icon={FaTrashAlt} onClick={() => alert('Eliminando')}>
+          Eliminar
+        </Dropdown.Item>
+      </Dropdown>
+    </div>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,12 @@
+import { closestCenter, DndContext } from '@dnd-kit/core';
 import TodoItem from './TodoItem';
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove
+} from '@dnd-kit/sortable';
 
-const TodoList = ({ todos, setTodos }) => {
+const TodoList = ({ todos = [], setTodos = () => {} }) => {
   const handleStatusChange = (id, newStatus) => {
     setTodos((prevTodos) =>
       prevTodos.map((todo) =>
@@ -9,18 +15,34 @@ const TodoList = ({ todos, setTodos }) => {
     );
   };
 
+  const handleDragEnd = (e) => {
+    const { active, over } = e;
+
+    setTodos((prevTodos) => {
+      const firstId = prevTodos.findIndex((todo) => todo.id === active.id);
+      const secondId = prevTodos.findIndex((todo) => todo.id === over.id);
+      const newOrder = arrayMove(todos, firstId, secondId);
+      return newOrder;
+    });
+  };
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          description={todo.description}
-          status={todo.status}
-          onStatusChange={handleStatusChange}
-        />
-      ))}
-    </ul>
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext
+        items={todos.map((todo) => todo.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            description={todo.description}
+            status={todo.status}
+            onStatusChange={handleStatusChange}
+          />
+        ))}
+      </SortableContext>
+    </DndContext>
   );
 };
 
