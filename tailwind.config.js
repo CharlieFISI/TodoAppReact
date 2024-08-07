240e0e37057b3f6e3dc8b26cc8b2ddd95634c790
LinuxChar <111671300+CharlieFISI@users.noreply.github.com>
2024-08-07 07:31:31 -0500

    Configuración del scrollbar
    Se añadió los estilos de la scrollbar, sin embargo, no se adecua correctamente la mención de -webkit-scrollbar en los distintos navegadores.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const flowbite = require('flowbite-react/tailwind');
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,5 +11,39 @@ module.exports = {
       }
     }
   },
-  plugins: [flowbite.plugin()]
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar': {
+          overflowY: 'hidden',
+          scrollbarWidth: 'thin',
+          scrollbarColor: '#dee2e6 transparent',
+          scrollbarGutter: 'stable both-edges'
+        },
+        '.scrollbar:hover': {
+          overflowY: 'auto'
+        },
+        '.scrollbar::-webkit-scrollbar': {
+          width: '8px',
+          height: '8px'
+        },
+        '.scrollbar::-webkit-scrollbar-button': {
+          display: 'none'
+        },
+        '.scrollbar::-webkit-scrollbar-track': {
+          backgroundColor: 'transparent',
+          borderRadius: '100vh'
+        },
+        '.scrollbar::-webkit-scrollbar-thumb': {
+          backgroundColor: '#343a40',
+          borderRadius: '100vh',
+          border: '3px solid transparent'
+        },
+        '.scrollbar::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: '#212529'
+        }
+      });
+    }),
+    flowbite.plugin()
+  ]
 };
