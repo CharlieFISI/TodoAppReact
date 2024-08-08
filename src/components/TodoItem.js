e6e5a293ca79c0f048c21cf3af16ffea984a2f3c
LinuxChar <111671300+CharlieFISI@users.noreply.github.com>
2024-08-08 14:45:32 -0500

    Actualización del componente
    Se añadió la función de eliminar y editar ToDo, también la creación de un modal para la edición de ToDo.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,13 @@
-import { Button, Checkbox, Label, Dropdown } from 'flowbite-react';
+import { useRef, useState } from 'react';
+import {
+  Button,
+  Checkbox,
+  Label,
+  Dropdown,
+  TextInput,
+  Select,
+  Modal
+} from 'flowbite-react';
 import {
   FaEllipsisH,
   FaBars,
@@ -8,26 +17,52 @@ import {
   FaAngleDown,
   FaAngleUp
 } from 'react-icons/fa';
-import { updateTodoStatus } from '../data';
+import { updateTodo, updateTodoStatus, deleteTodo } from '../data';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { restrictToParentElement } from '@dnd-kit/modifiers';
 
 const TodoItem = (props) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const inputRef = useRef(null);
+  const selectRef = useRef(null);
   const completedClass =
     props.status === 'completed' ? 'bg-gray-200 bg-opacity-40' : 'bg-gray-100';
 
   const getPriority = (priority) => {
     switch (priority) {
       case 'high':
-        return <FaAngleUp className="text-red-600 size-6" />;
+        return <FaAngleUp className="size-6 text-red-600" />;
       case 'medium':
-        return <FaMinus className="text-yellow-500 size-5" />;
+        return <FaMinus className="size-5 text-yellow-500" />;
       case 'low':
-        return <FaAngleDown className="text-green-600 size-6" />;
+        return <FaAngleDown className="size-6 text-green-600" />;
       default:
         return 'Desconocida';
     }
+  };
+
+  function onCloseModal() {
+    setOpenModal(false);
+    setErrorMessage('');
+  }
+
+  const handleTodoChange = async () => {
+    const description = inputRef.current.value;
+    const priority = selectRef.current.value;
+
+    if (!description) {
+      setErrorMessage('*Falta descripción');
+      return;
+    }
+
+    const result = await updateTodo(props.id, description, priority);
+
+    if (result === 'error') {
+      alert('Error actualizando la tarea');
+    }
+    props.onEdit(props.id, description, priority);
+    onCloseModal();
   };
 
   const handleCheckboxChange = async (e) => {
@@ -42,10 +77,18 @@ const TodoItem = (props) => {
     props.onStatusChange(props.id, newStatus);
   };
 
+  const handleDeleteToDo = async () => {
+    const result = await deleteTodo(props.id);
+
+    if (result === 'error') {
+      alert('Error eliminando la tarea');
+    }
+    props.onDelete(props.id);
+  };
+
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({
-      id: props.id,
-      modifiers: [restrictToParentElement]
+      id: props.id
     });
 
   const style = {
@@ -54,52 +97,108 @@ const TodoItem = (props) => {
   };
 
   return (
-    <div
-      style={style}
-      ref={setNodeRef}
-      {...attributes}
-      className={`my-1 flex w-full cursor-default snap-center snap-normal justify-between break-words rounded-md bg-white py-1 pl-px pr-2.5 shadow-sm ${completedClass}`}
-    >
-      <div className="flex items-center justify-items-start">
-        <Button
-          {...listeners}
-          as="span"
-          className="text-lg font-bold text-gray-400 bg-transparent border-none cursor-pointer hover:accent-black"
-        >
-          <FaBars />
-        </Button>
-        <Checkbox
-          className="mr-2 cursor-pointer focus:ring-transparent"
-          defaultChecked={props.status === 'completed'}
-          onChange={handleCheckboxChange}
-          color="dark"
-        />
-        <Label
-          className={`relative -top-px max-w-[calc(100%-40px)] break-words ${props.status === 'completed' ? 'line-through opacity-30' : ''}`}
-        >
-          {props.description}
-        </Label>
+    <>
+      <div
+        ref={setNodeRef}
+        style={style}
+        className={`my-1 flex w-full cursor-default snap-center snap-normal justify-between break-words rounded-md bg-white py-1 pl-px pr-2.5 shadow-sm ${completedClass}`}
+      >
+        <div className="flex items-center justify-items-start">
+          <Button
+            {...attributes}
+            {...listeners}
+            as="span"
+            className="cursor-move border-none bg-transparent text-lg font-bold text-gray-400 hover:accent-black focus:ring-transparent"
+          >
+            <FaBars />
+          </Button>
+          <Checkbox
+            className="mr-2 cursor-pointer focus:ring-transparent"
+            defaultChecked={props.status === 'completed'}
+            onChange={handleCheckboxChange}
+            color="dark"
+          />
+          <Label
+            className={`relative -top-px max-w-[calc(100%-40px)] break-words ${props.status === 'completed' ? 'line-through opacity-30' : ''}`}
+          >
+            {props.description}
+          </Label>
+        </div>
+        <div className="flex items-center justify-items-end space-x-3">
+          <Label className="grid size-6 place-content-center">
+            {getPriority(props.priority)}
+          </Label>
+          <Dropdown
+            label={<FaEllipsisH className="text-gray-600" />}
+            dismissOnClick={false}
+            placement="right-start"
+            inline
+            arrowIcon={false}
+          >
+            <Dropdown.Item icon={FaEdit} onClick={() => setOpenModal(true)}>
+              Editar
+            </Dropdown.Item>
+            <Dropdown.Item icon={FaTrashAlt} onClick={handleDeleteToDo}>
+              Eliminar
+            </Dropdown.Item>
+          </Dropdown>
+        </div>
       </div>
-      <div className="flex items-center space-x-3 justify-items-end">
-        <Label className="grid size-6 place-content-center">
-          {getPriority(props.priority)}
-        </Label>
-        <Dropdown
-          label={<FaEllipsisH className="text-gray-600" />}
-          dismissOnClick={false}
-          placement="right-start"
-          inline
-          arrowIcon={false}
-        >
-          <Dropdown.Item icon={FaEdit} onClick={() => alert('Editando')}>
-            Editar
-          </Dropdown.Item>
-          <Dropdown.Item icon={FaTrashAlt} onClick={() => alert('Eliminando')}>
-            Eliminar
-          </Dropdown.Item>
-        </Dropdown>
-      </div>
-    </div>
+      <Modal
+        show={openModal}
+        initialFocus={inputRef}
+        onClose={() => setOpenModal(false)}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="space-y-6">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Editar tarea
+            </h3>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="description" value="Descripción de la tarea" />
+              </div>
+              <TextInput
+                id="description"
+                defaultValue={props.description}
+                ref={inputRef}
+                placeholder="Inserte la descripción de la tarea"
+                required
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="priority"
+                  value="Seleccione la prioridad de la tarea"
+                />
+              </div>
+              <Select
+                id="priority"
+                defaultValue={props.priority}
+                ref={selectRef}
+                required
+              >
+                <option value="low">Baja</option>
+                <option value="medium">Media</option>
+                <option value="high">Alta</option>
+              </Select>
+            </div>
+            {errorMessage && (
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleTodoChange}>Guardar</Button>
+          <Button color="gray" onClick={() => setOpenModal(false)}>
+            Cancelar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
